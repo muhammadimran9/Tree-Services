d166362a3173e0e6c5478ec78c2e160b44d29d6a
IT Developer <malanhans777@gmail.com>
2026-02-21 11:59:38 +0500

    Add security headers for better SEO and security

--- next.config.js
+++ next.config.js
@@ -47,6 +47,31 @@ const nextConfig = {
   poweredByHeader: false,
   compress: true,
   generateEtags: false,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
